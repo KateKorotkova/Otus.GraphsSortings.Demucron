129dcce3f4900e7613973168b0661a7198b19a0d
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-08-06 16:15:57 +0300

    initial commit

---
+++ Otus.GraphsSortings.Demucron/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.GraphsSortings.Demucron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
